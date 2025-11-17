abd37e2a262daa3e833b4bce604fff22a7aef070
FWarsitz <fwarsitz@smail.uni-koeln.de>
2025-11-17 13:35:19 +0100

    H1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,29 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+		
+		int[] myArray = {5, 4, 3}; 
+		
+		int[] tempArray = new int[myArray.length];
+		
+		for (int i = 0; i < myArray.length; i++) {
+			
+			tempArray[i] = myArray[myArray.length - 1 - i];
+			
+		}
+		
+		myArray = tempArray;
+		
+		
+		for (int i = 0; i < myArray.length; i++) {
+			
+			System.out.println(myArray[i]);
+			
+		}
+
+	
+		
+	}
+	
+}
